dc127e37461e0b33e2feab3191bdca0ca7f0f159
Vincent Moore <devdebonair@gmail.com>
2017-04-13 16:35:36 -0400

    added employee id to ticket

--- Ticket.java
+++ Ticket.java
@@ -2,10 +2,12 @@ public class Ticket {
 	private Date datetime;
 	private HourType hourType;
 	private TicketType ticketType;
+	private int employeeId;
 
-	public Ticket(Date datetime, HourType hourType, TicketType ticketType) {
+	public Ticket(Date datetime, HourType hourType, TicketType ticketType, int employeeId) {
 		this.datetime = datetime;
 		this.hourType = hourType;
 		this.ticketType = ticketType;
+		this.employeeId = employeeId
 	}
 }
