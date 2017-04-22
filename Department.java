d8ab67f53a75727ba0e8c469e4ab3f9d1429079d
Vincent Moore <devdebonair@gmail.com>
2017-04-21 20:21:30 -0400

    changed ticket data structure to Hashmap of ArrayList<Ticket>

--- Department.java
+++ Department.java
@@ -1,12 +1,13 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Department {
 	int code;
 	DepartmentType type;
 	HashMap<Int, Employee> employees;
-	HashMap<Date, Ticket> tickets;
+	HashMap<Date, ArrayList<Ticket>> tickets;
 
-	public Department(int code, DepartmentType type, HashMap<Int, Employee> employees, HashMap<Date, Ticket> tickets) {
+	public Department(int code, DepartmentType type, HashMap<Int, Employee> employees, HashMap<Date, ArrayList<Ticket>> tickets) {
 		this.code = code;
 		this.type = type;
 		this.employees = employees;
@@ -14,6 +15,14 @@ public class Department {
 	}
 
 	public Department(int code, DepartmentType type) {
-		this(code, type, new HashMap<Int, Employee>(), new HashMap<Date, Ticket>());
+		this(code, type, new HashMap<Int, Employee>(), new HashMap<Date, ArrayList<Ticket>>());
+	}
+
+	public Employee getEmployee(int id) {
+		return employees.get(id);
+	}
+
+	public ArrayList<Tickets> getEmployeeTickets() {
+		
 	}
 }
