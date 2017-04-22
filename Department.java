a6859ed2c2ea8c2b288270d52b203be5f702d757
Vincent Moore <devdebonair@gmail.com>
2017-04-21 20:00:19 -0400

    added employees and tickets to department

--- Department.java
+++ Department.java
@@ -1,9 +1,19 @@
+import java.util.HashMap;
+
 public class Department {
 	int code;
 	DepartmentType type;
+	HashMap<Int, Employee> employees;
+	HashMap<Date, Ticket> tickets;
 
-	public Department(int code, DepartmentType type) {
+	public Department(int code, DepartmentType type, HashMap<Int, Employee> employees, HashMap<Date, Ticket> tickets) {
 		this.code = code;
 		this.type = type;
+		this.employees = employees;
+		this.tickets = tickets;
+	}
+
+	public Department(int code, DepartmentType type) {
+		this(code, type, new HashMap<Int, Employee>(), new HashMap<Date, Ticket>());
 	}
 }
